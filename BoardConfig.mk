8a0d3e97b7a26cec8bd7a279722994e533951b65
Danny Baumann <dannybaumann@web.de>
2017-01-25 17:44:12 +0100

    r7plus: Switch to Spectrum fingerprint TZ app.
    This means using a blob fingerprint sensor HAL, at least for the moment.
    
    Change-Id: I45c6147ceb0ec23965a7c75c345fa12e0299dd1e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,9 @@ TARGET_USES_UNCOMPRESSED_KERNEL := false
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000
 TARGET_KERNEL_CONFIG := lineageos_r7plus_defconfig
 
+# libc config (needed by fingerprint HAL)
+TARGET_NEEDS_GCC_LIBC := true
+
 # Partition info
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
