d01c1b38bfd21ed698c2c8db11c577d111e1657b
Danny Baumann <dannybaumann@web.de>
2017-01-25 17:44:12 +0100

    r7plus: Support Spectrum fingerprint TZ app.
    This means using a blob fingerprint sensor HAL, at least for the moment.
    
    Change-Id: I45c6147ceb0ec23965a7c75c345fa12e0299dd1e

--- fingerprint/Android.mk
+++ fingerprint/Android.mk
@@ -2,17 +2,36 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := fingerprint_wrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libdl \
+	libutils \
+	libhardware
+
+LOCAL_MODULE := fingerprint.spectrum
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MULTILIB := 64
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := \
 	fpc1020_hal.cpp \
 	Fpc1020Sensor.cpp \
 	QSEEComApp.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-        liblog \
-        libdl \
+	liblog \
+	libdl \
 	libutils
 
-LOCAL_MODULE := fingerprint.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := fingerprint.coloros
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_MODULE_TAGS := optional
