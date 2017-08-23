f3d27e593ebc5bbbfacd65723fcbec802f2faf76
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-24 01:04:57 +0800

    r7plus: Replace vendor/cm with vendor/lineage
    Change-Id: Iacfc55a2d132ab488f57fe8a70cf29457b17fa1e

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oppo/r7plus/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_r7plus
 PRODUCT_DEVICE := r7plus
