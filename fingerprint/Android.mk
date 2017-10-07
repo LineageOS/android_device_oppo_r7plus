ac3be137489db3166f4f45531f3ee9f20ca06282
Michael Bestas <mkbestas@lineageos.org>
2017-10-07 18:05:05 +0300

    r7plus: Build only 64bit version of fp HAL
    * 32bit one is useless
    
    Change-Id: If9c5dd46f778bfd6bea22b77e5da7ef6f2c426a2

--- fingerprint/Android.mk
+++ fingerprint/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -15,5 +16,7 @@ LOCAL_MODULE := fingerprint.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_MULTILIB := 64
 
 include $(BUILD_SHARED_LIBRARY)
