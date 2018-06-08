94e6ab832d73a1b387d8ce91168dcc28c400f9fd
Danny Baumann <dannybaumann@web.de>
2018-06-08 08:46:25 +0200

    r7plus: Build fingerprint HIDL service
    * Also remove fingerprint service
    
    Change-Id: Ib1d7dd1e60c5062885138948f8f465e80d2dcbf5

--- device.mk
+++ device.mk
@@ -63,8 +63,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    fingerprint.msm8916 \
-    fingerprintd
+    fingerprint.msm8916
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # Input configuration
 PRODUCT_COPY_FILES += \
