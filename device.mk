4791e36c01b83ae4acc69872e11e0ed984a87632
Danny Baumann <dannybaumann@web.de>
2018-09-03 16:23:16 +0200

    Use correct dalvik heap configuration.
    Copied from the 15.1 phone-xxhdpi-3072-dalvik-heap.mk file.
    
    Change-Id: If1a99ed799eb06f4214f5fe86a5a68172463b615

--- device.mk
+++ device.mk
@@ -45,7 +45,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
 
 # Density
 PRODUCT_AAPT_CONFIG := normal
