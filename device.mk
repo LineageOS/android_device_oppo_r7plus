6ff9b4e5e266e69bf6904574f2af2ffaa6cbe40c
Michael Bestas <mkbestas@lineageos.org>
2019-01-04 16:43:17 +0200

    r7plus: Move dalvik props to device tree
    * Also switch to 2gb dalvik values as the 3gb one causes issues
    
    Change-Id: I1a03943d0d696ca1827d1f89666bf2ce360a09a5

--- device.mk
+++ device.mk
@@ -45,7 +45,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
 
 # Density
 PRODUCT_AAPT_CONFIG := normal
